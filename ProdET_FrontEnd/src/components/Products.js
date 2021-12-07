c4427586d8f8475b717411f1856c9a0ae4a666fe
michelangelo-granato <391michelangelo@gmail.com>
2021-12-06 19:46:13 -0500

    formatting and removed comment from app.js

--- ProdET_FrontEnd/src/components/Products.js
+++ ProdET_FrontEnd/src/components/Products.js
@@ -1,29 +1,28 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/product";
 
-
 const Products = (props) => {
-    //const [x,setX] = useState(0)
-    //setX(5)
+  //const [x,setX] = useState(0)
+  //setX(5)
 
-    useEffect(()=>{
-        props.fetchAllProducts()
-    },[])//componentDidMount equivalent with hooks
+  useEffect(() => {
+    props.fetchAllProducts();
+  }, []); //componentDidMount equivalent with hooks
 
-    return ( <div>from Products</div>);
-}
+  return <div>from Products</div>;
+};
 
-const mapStateToProps = state => {
-    return {
-        productList : state.product.list
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    productList: state.product.list,
+  };
+};
 
 const mapActionToProps = {
-    fetchAllProducts : actions.fetchAll
-}
+  fetchAllProducts: actions.fetchAll,
+};
 
 //connect is a function that returns another function,
 //we pass Products in as param
-export default  connect(mapStateToProps,mapActionToProps)(Products);
+export default connect(mapStateToProps, mapActionToProps)(Products);
--- ProdET_FrontEnd/src/reducers/product.js
+++ ProdET_FrontEnd/src/reducers/product.js
@@ -1,19 +1,18 @@
 import { ACTION_TYPES } from "../actions/product";
 
-const initialState={
-    list:[]
-}
+const initialState = {
+  list: [],
+};
 
-export const product = (state = initialState,action) =>{
-    switch (action.type) {
-        case ACTION_TYPES.FETCH_ALL:
-            return{
-                ...state,
-                list:[...action.payload]
-            }
-            
-    
-        default:
-            return state;
-    }
-}
+export const product = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.FETCH_ALL:
+      return {
+        ...state,
+        list: [...action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
